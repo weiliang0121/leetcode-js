119f115343eaacab458b12e5d3a66e1f5d0402f6
weiliang0121 <weilianghit@163.com>
2020-08-08 11:54:06 +0800

    feat: add reverse

--- src/easy/reverse.js
+++ src/easy/reverse.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var reverse = function(x) {
+  const max = Math.pow(2, 31) - 1;
+  const min = -Math.pow(2, 31);
 
+  let rev = 0;
+  let pop = 0;
+  while(x !== 0) {
+    pop = x % 10;
+    x = parseInt(x / 10, 10);
+
+    if ((rev > parseInt(max / 10, 10)) || (rev === parseInt(max / 10, 10) && pop > 7)) return 0;
+    if ((rev < parseInt(min / 10, 10)) || (rev === parseInt(min / 10, 10) && pop < -8)) return 0;
+
+    rev = rev * 10 + pop;
+  }
+
+  return rev;
 };
